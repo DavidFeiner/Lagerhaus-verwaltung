52a917e416ca97ffb432c37fadd71361b7e1dbd4
David Feiner <david.feiner@hak-kitz.ac.at>
2023-03-31 09:32:55 +0200

    Sell Commit

--- Lagerverwaltung/sell.cs
+++ Lagerverwaltung/sell.cs
@@ -12,9 +12,17 @@ namespace Lagerverwaltung
 {
     public partial class sell : Form
     {
+        DataTable table;
+        SQLCommunication cmc = new SQLCommunication();
         public sell()
         {
             InitializeComponent();
+            DataTable dataTable = cmc.DataOverview(dataGridView1);
+            this.table = dataTable;
+            int Value = 5;
+            LabelValue.Text = "Value: " + Value;
+            decimal Money = 50000;
+            LabelMoney.Text = "Money: " + Money + " Euro";
         }
 
         private void bttn_back_Click(object sender, EventArgs e)
@@ -34,12 +42,28 @@ namespace Lagerverwaltung
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
-
+            cmc.Load(table);
         }
 
         private void ProduktCmbx_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
+
+
+        private void LabelValue_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void LabelMoney_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sell_Load(object sender, EventArgs e)
+        { }
     }
 }
